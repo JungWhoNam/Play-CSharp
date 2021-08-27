66e3c79b6992808d45d7de0967999b6886d84dd1
JungWho Nam <jungwhonam@gmail.com>
2021-08-27 11:00:54 +0900

    #Learn getter and setter properties

---
+++ Basic/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningCSharp
+{
+    class Character
+    {
+        private int hp;
+
+        public int Hp
+        {
+            get { return hp; }
+            set
+            {
+                if (value < 0 || value > 100) hp = 0;
+                else hp = value;
+            }
+        }
+    }
+}
---
+++ Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character = new Character();
+
+            character.Hp = 10;
+            Console.WriteLine("HP is " + character.Hp);
+
+            character.Hp = -10;
+            Console.WriteLine("HP is " + character.Hp);
+
+            character.Hp = 110;
+            Console.WriteLine("HP is " + character.Hp);
+        }
+    }
+}
