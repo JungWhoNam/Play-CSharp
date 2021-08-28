468aa7332eeb6e7e0933017becc6fae3f89c377f
JungWho <jungwhonam@gmail.com>
2021-08-28 14:17:23 +0900

    #Learn const and readonly keywords

--- Basic/Character.cs
+++ Basic/Character.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
+// getter and setter properties
+// const vs readonly keywords
 namespace LearningCSharp
 {
     class Character
     {
+        // can only assign values in declaration part and in the constructor part (runtime constant)
+        // cannot declare the keyword in methods (only in the constructor)
+        // can be used with static modifiers
+        public static readonly long baseline;
+        public readonly string name = "John";
+
+        // can only assign values in declaration part (compile constant)
+        // can declare the keyword inside methodss
+        // cannot be used with static modifiers
+        public const string type = "Human";
+
         private int hp;
 
+        // getter and setter properties
         public int Hp
         {
             get { return hp; }
@@ -17,5 +31,21 @@ namespace LearningCSharp
                 else hp = value;
             }
         }
+
+        static Character()
+        {
+            baseline = DateTime.Now.Ticks;
+        }
+
+        public Character()
+        {
+        }
+
+        public Character(string name)
+        {
+            // can reassign values in the constructor because the variable has readonly keyword (if it has constant keyword wouldn't work).
+            this.name = name;
+        }
+
     }
 }
--- Basic/Program.cs
+++ Basic/Program.cs
@@ -6,16 +6,41 @@ namespace LearningCSharp
     {
         static void Main(string[] args)
         {
-            Character character = new Character();
+            {
+                Character character = new Character();
 
-            character.Hp = 10;
-            Console.WriteLine("HP is " + character.Hp);
+                character.Hp = 10;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
 
-            character.Hp = -10;
-            Console.WriteLine("HP is " + character.Hp);
+                character.Hp = -10;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
 
-            character.Hp = 110;
-            Console.WriteLine("HP is " + character.Hp);
+                character.Hp = 110;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
+
+                Console.WriteLine("{0} starts at {1}", character.GetType().Name, Character.baseline);
+                // const are implicity static
+                // so access them through the class name (instead through instance members)
+                Console.WriteLine("{0} is {1}", character.GetType().Name, Character.type);
+            }
+
+            {
+                Character character = new Character("Tonny");
+
+                character.Hp = 10;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
+
+                character.Hp = -10;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
+
+                character.Hp = 110;
+                Console.WriteLine("{0} has a health point of {1}", character.name, character.Hp);
+
+                Console.WriteLine("{0} starts at {1}", character.GetType().Name, Character.baseline);
+                // const are implicity static
+                // so access them through the class name (instead through instance members)
+                Console.WriteLine("{0} is {1}", character.GetType().Name, Character.type);
+            }
         }
     }
 }
