11bb5f3c83d1167f442005d902c16003e6ed4f2d
JungWho <jungwhonam@gmail.com>
2021-09-02 12:54:52 +0900

    Learn boxing and unboxing

--- Explore/Program.cs
+++ Explore/Program.cs
@@ -16,15 +16,35 @@ namespace LearningCSharp.Explore
     {
         static void Main(string[] args)
         {
-            WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+            //WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
 
-            WriteLine(System.Math.PI);
+            //WriteLine(System.Math.PI);
 
-            WriteLine(PI);
+            //WriteLine(PI);
 
-            WriteLine(s.Format("Area: {0:N2}\n", PI));
+            //WriteLine(s.Format("Area: {0:N2}\n", PI));
 
-            ReadLine();
+            //ReadLine();
+
+            // boxing and unboxing
+            {
+                // boxing a value type... converts the value type to the type object (creates an object and copies the value). 
+                // This create an object reference "o" on the stack that references a value of the type int on the heap.
+                // so in the below example, the references of "i" and "o" will be different (even with implicit boxing). 
+
+                int i = 123; // on the stack
+                object o1 = i; // implicit boxing
+                object o2 = (object)i; // explicit boxing
+
+
+                WriteLine(ReferenceEquals(i, o1));
+                WriteLine(ReferenceEquals(i, o2));
+
+                i = (int)o1; //unboxing
+                WriteLine(ReferenceEquals(i, o1));
+            }
         }
     }
 }
+
+// stack and heap
