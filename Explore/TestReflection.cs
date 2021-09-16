6361b37e2c17f5318b3288a344219a936a736878
JungWho Nam <jungwhonam@gmail.com>
2021-09-16 14:17:03 +0900

    Learn about Reflection

---
+++ Explore/TestReflection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Explore
+{
+    class TestReflection
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Type type = a.GetType();
+
+            {
+                FieldInfo[] fields = type.GetFields();
+
+                foreach (FieldInfo field in fields)
+                {
+                    Console.WriteLine("Type:{0}, Name:{1}", field.FieldType.Name, field.Name);
+                }
+            }
+            Console.WriteLine();
+
+            {
+                MethodInfo[] infos = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+                foreach (MethodInfo info in infos)
+                {
+                    Console.WriteLine("Type:{0}, Name:{1}", info.ReturnType.Name, info.Name);
+                }
+            }
+        }
+
+    }
+}
