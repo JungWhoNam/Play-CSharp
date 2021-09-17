4bbbfb8208c6316be51531374e82fa56d611520f
JungWho Nam <jungwhonam@gmail.com>
2021-09-17 14:55:38 +0900

    Learn about Reflection - create an instance of type

--- Explore/TestReflection.cs
+++ Explore/TestReflection.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 
 namespace LearningCSharp.Explore
 {
+    // https://qzqz.tistory.com/242?category=752329
     class TestReflection
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            Type type = a.GetType();
-
             {
+                int a = 0;
+                Type type = a.GetType();
+
                 FieldInfo[] fields = type.GetFields();
 
                 foreach (FieldInfo field in fields)
@@ -25,13 +26,67 @@ namespace LearningCSharp.Explore
             Console.WriteLine();
 
             {
+                int a = 0;
+                Type type = a.GetType();
+
                 MethodInfo[] infos = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
                 foreach (MethodInfo info in infos)
                 {
                     Console.WriteLine("Type:{0}, Name:{1}", info.ReturnType.Name, info.Name);
                 }
             }
+            Console.WriteLine();
+
+            {
+                Type type = Type.GetType("LearningCSharp.Explore.Profile");
+                MethodInfo methodInfo = type.GetMethod("Print");
+                PropertyInfo nameProperty = type.GetProperty("Name");
+                PropertyInfo phoneProperty = type.GetProperty("Phone");
+
+                object profile = Activator.CreateInstance(type, "김나다", "123-4321");
+                methodInfo.Invoke(profile, null);
+
+                profile = Activator.CreateInstance(type);
+                nameProperty.SetValue(profile, "가나다", null);
+                phoneProperty.SetValue(profile, "123-4567", null);
+                methodInfo.Invoke(profile, null);
+
+            }
+        }
+    }
+
+    class Profile
+    {
+        private string name;
+        private string phone;
+
+        public Profile()
+        {
+            name = ""; phone = "";
         }
 
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("{0}, {1}", name, phone);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
     }
+
 }
