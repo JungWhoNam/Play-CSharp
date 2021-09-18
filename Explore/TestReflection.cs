916d58a11bb8f427b13bf33bb01283912002c29c
JungWho <jungwhonam@gmail.com>
2021-09-18 14:22:57 +0900

    Learn about reflection - dynamically create an instance

--- Explore/TestReflection.cs
+++ Explore/TestReflection.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LearningCSharp.Explore
 {
     // https://qzqz.tistory.com/242?category=752329
+    // https://nowonbun.tistory.com/488
     class TestReflection
     {
         static void Main(string[] args)
@@ -17,7 +19,6 @@ namespace LearningCSharp.Explore
                 Type type = a.GetType();
 
                 FieldInfo[] fields = type.GetFields();
-
                 foreach (FieldInfo field in fields)
                 {
                     Console.WriteLine("Type:{0}, Name:{1}", field.FieldType.Name, field.Name);
@@ -43,14 +44,20 @@ namespace LearningCSharp.Explore
                 PropertyInfo nameProperty = type.GetProperty("Name");
                 PropertyInfo phoneProperty = type.GetProperty("Phone");
 
+                // 특정 형식의 인스턴스를 동적으로 만듬
                 object profile = Activator.CreateInstance(type, "김나다", "123-4321");
+                // 동적으로 메소드 호출
                 methodInfo.Invoke(profile, null);
 
+                // 동적으로 프로퍼티 및 인덱서 정보 담기
                 profile = Activator.CreateInstance(type);
                 nameProperty.SetValue(profile, "가나다", null);
                 phoneProperty.SetValue(profile, "123-4567", null);
                 methodInfo.Invoke(profile, null);
+            }
 
+            {
+                AssemblyName aName = new AssemblyName("DynamicAssemblyExample");
             }
         }
     }
